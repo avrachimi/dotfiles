ba0c6bbfecc53be2044f2035f42fbb87cccaeeca
Andreas Vrachimi <avrachimi@hotmail.com>
2024-09-16 10:06:20 +0100

    Updated [j/t]sx formatters

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -2,9 +2,11 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Use a sub-list to run only the first available formatter
-		javascript = { "prettier", "prettierd", stop_after_first = true },
-		typescript = { "prettier", "prettierd", stop_after_first = true },
-		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		-- running prettier first for React Native project
+		typescriptreact = { "prettier", "prettierd", stop_after_first = true },
+		javascriptreact = { "prettier", "prettierd", stop_after_first = true },
 		-- Run multiple formatters sequentially
 		go = { "goimports", "gopls" },
 		rust = { "rustfmt" },
